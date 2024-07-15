f2fc1affe00e0dfb1aedaf313fe5f95cb32c9d5f
Daniel Hernandez <danielhd94@hotmail.com>
2024-07-15 08:46:51 -0700

    fix: change time zone to date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def format_as_localtime(datetime, format = '')
     format = "%d/%b/%Y, %H:%M:%S" unless format.present?
-    datetime.localtime.strftime format
+    datetime.in_time_zone(Time.zone).strftime(format)
   end
 end
